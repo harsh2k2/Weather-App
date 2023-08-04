f15a3596a9b9acef9880291ac7e18d332c3aaeb1
Harsh Kumar <harsh622k@gmail.com>
2023-08-04 22:42:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let target = "delhi";
 // Function to fetch Data from Weather API
 const fetchData = async (target) => {
   try {
-    const url = `https://api.weatherapi.com/v1/current.json?key=5b27a6ef3547402582e62007222306&q=${target}`;
+    const url = `https://api.weatherapi.com/v1/current.json?key=2a9470ab045c4474b3a170844230408=${target}`;
 
     const response = await fetch(url);
     const data = await response.json();
